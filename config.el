c79706696dd4de587ad619eeac2a282d8442ec34
Michael H Wang <mwang5@atlassian.com>
2023-01-06 18:05:15 +1100

    update restructure the config file

--- config.el
+++ config.el
@@ -1,113 +1,22 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Michael Wang"
       user-mail-address "michael@wonng.com")
 
 ;; CHANGE LEADER KEY
 (setq ;doom-leader-key ","
-      doom-localleader-key "\\")
+ doom-localleader-key "\\")
 
 ;; set transparentcy
 (defvar efs/frame-transparency '(95 . 95))
 
-;; add ECS to quit
-(defun minibuffer-keyboard-quit ()
-  "Abort recursive edit.
-In Delete Selection mode, if the mark is active, just deactivate it;
-then it takes a second \\[keyboard-quit] to abort the minibuffer."
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-      (setq deactivate-mark  t)
-    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
-    (abort-recursive-edit)))
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-(global-set-key [escape] 'evil-exit-emacs-state)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-(setq doom-font (font-spec :family "OperatorMonoSSmLig Nerd Font Mono" :size 14.0 :weight 'light))
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-palenight)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/MEGA/orgs/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; ;; they are implemented.
-;; (after! org (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)"))
-;;          org-todo-keywords-for-agenda '((sequence "TODO" "WAITING" "|" "DONE" "CANCELED"))
-;; ))
-
-;; ;;(setq telega-server-libs-prefix "/opt/homebrew/opt/tdlib")
-
-;; (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-;; (setq default-frame-alist '( (left . 0) (top . 0) (height . 65) (width . 100) ))
+(setq doom-font (font-spec :family "OperatorMonoSSmLig Nerd Font Mono" :size 18.0 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "Merriweather" :weight 'normal)
+      doom-serif-font (font-spec :family "OperatorMonoSSmLig Nerd Font Mono" :weight 'light)
+      doom-theme 'doom-palenight
+      display-line-numbers-type t
+      org-directory "~/orgs/"
+      )
 
 ;; center the frame
 (defun my/frame-recenter (&optional frame)
@@ -153,47 +62,84 @@ If FRAME is omitted or nil, use currently selected frame."
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(setq org-hide-emphasis-markers t)
-(use-package org-bullets
-    :ensure t
-    :config
-    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-(font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+(setq org-ellipsis " ▾")
 
-;; Set faces for heading levels
-  (dolist (face '((org-document-title . 2.0)
-                  (org-level-1 . 2.4)
-                  (org-level-2 . 1.8)
-                  (org-level-3 . 1.6)
-                  (org-level-4 . 1.5)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.05)
-                  (org-level-7 . 1.0)
-                  (org-level-8 . 1.0)))
-        (set-face-attribute (car face) nil :font "Merriweather" :weight 'normal :height (cdr face)))
+(defun my/org-init-settings ()
+  (org-superstar-mode t)
+  ;; improve org mode looks
+  (setq org-startup-indented t
+        org-pretty-entities t
+        org-hide-emphasis-markers t
+        org-startup-with-inline-images t
+        org-src-fontify-natively t
+        org-image-actual-width '(300))
+  )
 
-;; Ensure that anything that should be fixed-pitch in Org files appears that way
-(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-(set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-(set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)
+;; (font-lock-add-keywords 'org-mode
+;;                         '(("^ *\\([-]\\) "
+;;                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-;; turn auto save off
+;; (setq org-bullets-bullet-list '("►" "▸" "•" "★" "◇" "◇" "◇" "◇"))
+
+
+(setq org-superstar-remove-leading-stars t)
+(setq org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●"))
+(setq org-superstar-item-bullet-alist '("•"))
+
+;; (use-package org-bullets
+;;     :ensure t
+;;     :config
+;;     (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(defun my/org-font-settings ()
+  ;; Increase the size of various headings
+  ;; Set faces for heading levels
+  (set-face-attribute 'org-document-title nil :font "Merriweather" :weight 'bold :height 1.3)
+  (set-face-attribute 'org-level-1 nil :font "Merriweather" :weight 'bold :height 1.8 :foreground "PaleVioletRed1")
+  (set-face-attribute 'org-level-2 nil :font "Merriweather" :weight 'bold :height 1.3 :foreground "PaleVioletRed2")
+  (set-face-attribute 'org-level-3 nil :font "Merriweather" :weight 'bold :height 1.05 :foreground "PaleVioletRed3")
+  (set-face-attribute 'org-level-4 nil :font "Merriweather" :weight 'bold :height 1.0)
+  (set-face-attribute 'org-level-5 nil :font "Merriweather" :weight 'bold :height 1.1)
+  (set-face-attribute 'org-level-6 nil :font "Merriweather" :weight 'bold :height 1.1)
+  (set-face-attribute 'org-level-7 nil :font "Merriweather" :weight 'bold :height 1.1)
+  (set-face-attribute 'org-level-8 nil :font "Merriweather" :weight 'bold :height 1.1)
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-link nil     :inherit 'fixed-pitch :foreground "SkyBlue1" :underline t)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :foreground "PaleVioletRed1" :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+  ;; (set-face-attribute 'org-superstar-header-bullet nil :inherit 'fixed-pitch)
+  ;; (set-face-attribute 'org-superstar-leading-bullet nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+
+(defun my/style-org ()
+                                        ;(setq line-spacing 0.1)
+  (my/org-font-settings)
+  (my/org-init-settings)
+  (doom/reload-font)
+  )
+
+(add-hook 'org-mode-hook 'my/style-org)
+
+;; ;; turn auto save off
 (setq auto-save-default nil)
 
 ;; auto wrap the line over 80 column
 (global-visual-line-mode t)
 
 (setq doom-themes-treemacs-theme "doom-colors")
+(setq doom-themes-treemacs-enable-variable-pitch nil)
+
 
 ;; set transparency
 (set-frame-parameter (selected-frame) 'alpha efs/frame-transparency)
@@ -220,11 +166,12 @@ If FRAME is omitted or nil, use currently selected frame."
   (blamer-min-offset 70)
   :custom-face
   (blamer-face ((t :foreground "#7a88cf"
-                    :background nil
-                    :height 120
-                    :italic t)))
+                   :background nil
+                   :height 120
+                   :italic t)))
   :config
   (global-blamer-mode 1))
 (setq blamer-view 'overlay)
 (setq blamer-author-formatter "  ✎ %s ")
 (setq blamer-commit-formatter " ● %s")
+(setq tree-sitter-hl-mode t)
--- packages.el
+++ packages.el
@@ -9,10 +9,11 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! telega)
-(package! org-bullets)
+;;(package! org-bullets)
 (package! treemacs-all-the-icons)
 (package! tide)
 (package! blamer)
+(package! org-superstar)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
